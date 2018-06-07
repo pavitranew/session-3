36bcb925bd6faee0ff513ab31fe61461103aff5a
DannyBoyNYC <DannyBoyNYC>
2018-06-07 15:54:36 -0400

    done

--- app/js/main.js
+++ app/js/main.js
@@ -1,39 +1,65 @@
-// create the dynamic nav based on the navItems array
 const nav = document.getElementById('main');
-const navLinks = nav.querySelector('#nav-links');
-const markup = `${navItems.map(listItem => `<li><a href="${listItem.link}">${listItem.label}</a></li>`).join('')}`;
-navLinks.innerHTML = markup;
+const navbar = nav.querySelector('.navitems');
+const siteWrap = document.querySelector('.site-wrap');
 
-// const logo = document.querySelector('#main ul li');
-// logo.classList.add('logo');
-// logo.firstChild.innerHTML = '<img src="img/logo.svg" />';
+fetchData(null, function (content) {
+  const markup =
+    `<ul>
+    ${content.map(
+      listItem => `<li><a href="${listItem.link}">${listItem.label}</a></li>`
+    ).join('')}
+    </ul>`;
+  nav.innerHTML = markup;
 
-// sticky nav
+  // const logo = document.querySelector('#main ul li');
+  // logo.classList.add('logo');
+  // logo.firstChild.innerHTML = '<img src="img/logo.svg" />';
+  
+})
+
 let topOfNav = nav.offsetTop;
 
-window.addEventListener('scroll', fixNav)
-
 function fixNav() {
-  if (window.scrollY >= topOfNav) {
+  if(window.scrollY >= topOfNav) {
     document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('fixed-nav')
+    document.body.classList.add('fixed-nav');
   } else {
+    document.body.classList.remove('fixed-nav');
     document.body.style.paddingTop = 0;
-    document.body.classList.remove('fixed-nav')
   }
 }
 
-// hashes
-const siteWrap = document.querySelector('.site-wrap');
 
-window.onhashchange = function() {
+function navigate() {
   let newloc = window.location.hash;
-  let newContent = navItems.filter(
-    navItem => navItem.link == newloc
-  )
-  siteWrap.innerHTML = `
+  fetchData(newloc, function (content) {
+    let newContent = content.filter( contentItem => contentItem.link == newloc );
+    siteWrap.innerHTML = `
     <h2>${newContent[0].header}</h2>
+    ${newContent[0].image}
     ${newContent[0].content}
-  `
+    `;
+  })
 }
 
+
+function fetchData(hash, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.onload = function () {
+    callback(JSON.parse(xhr.response));
+  };
+  
+  xhr.open('GET', 'http://localhost:3004/content', true);
+  xhr.send();
+}
+
+
+if (!location.hash) {
+  location.hash = '#watchlist';
+}
+
+navigate();
+
+window.addEventListener('scroll', fixNav);
+window.addEventListener('hashchange', navigate);
