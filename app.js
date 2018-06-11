187dd4fcc46ff9edfb04c8cde64f51e25c160842
DannyBoyNYC <DannyBoyNYC>
2018-06-11 16:31:48 -0400

    done

--- app.js
+++ app.js
@@ -2,25 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
-
 const port = 9000;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('app'));
 
 app.get('/', (req, res) => {
-  db
-    .collection('entries')
-    .find()
-    .toArray((err, result) => {
-      res.sendFile(__dirname + '/index.html');
-    });
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/entries', (req, res) => {
   db.collection('entries').save(req.body, (err, result) => {
-    if (err) return console.log(err);
-    console.log('saved to database');
     res.redirect('/');
   });
 });
