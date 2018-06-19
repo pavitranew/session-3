f85673a70507bcab09fb35d756ea4a2a65971a46
DannyBoyNYC <DannyBoyNYC>
2018-06-19 16:22:56 -0400

    done

--- app.js
+++ app.js
@@ -5,51 +5,18 @@ const app = express();
 const port = 9000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
 
-// app.use(express.static('app'));
-
-app.get('/', (req, res) => {
-  db
-    .collection('entries')
-    .find()
-    .toArray((err, result) => {
-      if (err) return console.log(err);
-      // renders index.ejs
-      res.render('index.ejs', { entries: result });
-    });
-});
+app.use(express.static('app'));
 
 app.post('/entries', (req, res) => {
   db.collection('entries').save(req.body, (err, result) => {
-    if (err) return console.log(err);
-    console.log('saved to database');
     res.redirect('/');
   });
 });
-// app.get('/reverse/:name', (req, res) => {
-//   const reverse = [...req.params.name].reverse().join('');
-//   res.send(reverse)
-// });
-
-// app.get('/watchlist', function(req, res) {  // our second route
-//   res.send(`
-//     <h1>Watchlist</h1>
-//     <p>Commentary on Watchlists will go here.</p>
-//     `);
-// });
-
-// app.get('/entry/:name', function(req, res) {
-//   let name = req.params.name;
-//   res.send(`
-//     <h1>${name}</h1>
-//     <p>Commentary on ${name} will go here.</p>
-//     `);
-// });
 
 app.get('*', function(req, res){
   res.send(`
-    <h1>Page not found</h1>
+    <h1>Oopsy! Page not found</h1>
     `)
 })
 
